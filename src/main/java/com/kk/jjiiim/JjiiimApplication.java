12e0dbbed094268a9a047d327a1c72a0e13dd223
kyoohyun <kgh2120@naver.com>
2023-06-15 15:18:18 +0900

    init project

---
+++ src/main/java/com/kk/jjiiim/JjiiimApplication.java
@@ -0,0 +1,13 @@
+package com.kk.jjiiim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjiiimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JjiiimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kk/jjiiim/JjiiimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kk.jjiiim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjiiimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
