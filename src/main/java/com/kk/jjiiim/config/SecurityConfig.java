1e9faf172cf780ff97cebcfefbd461e820f94b20
kyoohyun <kgh2120@naver.com>
2023-06-23 14:17:33 +0900

    config : security 설정 변경
    #9

--- src/main/java/com/kk/jjiiim/config/SecurityConfig.java
+++ src/main/java/com/kk/jjiiim/config/SecurityConfig.java
@@ -10,26 +10,28 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
+//                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf.ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).disable())
                 .headers(headers ->
                         headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .sessionManagement(sessionManager -> sessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(request -> request.requestMatchers("/common/**", "/customer/signup", "/manager/signup").permitAll());
-
-
-
-
-
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers(AntPathRequestMatcher.antMatcher(
+                                "/h2-console/**")).permitAll()
+                                .requestMatchers("/common/**",
+                                        "/customer/signup", "/manager/signup").permitAll()
+                               );
         return http.build();
     }
 }
